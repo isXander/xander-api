584ddeca43b592345e638af43d722cc3928a3abf
isXander <xandersmith2008@gmail.com>
2022-03-04 14:01:47 -0500

    add env variable for mongodb address name

--- src/main/kotlin/dev/isxander/metricsapi/backend/MongoDB.kt
+++ src/main/kotlin/dev/isxander/metricsapi/backend/MongoDB.kt
@@ -2,10 +2,11 @@ package dev.isxander.metricsapi.backend
 
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import dev.isxander.metricsapi.utils.MONGO_ADDRESS
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
 
-val mongoClient = KMongo.createClient()
+val mongoClient = KMongo.createClient(MONGO_ADDRESS)
 val database = mongoClient.getDatabase("metrics")
 
 inline fun <reified T : Any> MongoDatabase.getApplicationCollection(application: String, collection: String): MongoCollection<T> =
--- src/main/kotlin/dev/isxander/metricsapi/utils/Constants.kt
+++ src/main/kotlin/dev/isxander/metricsapi/utils/Constants.kt
@@ -1,3 +1,4 @@
 package dev.isxander.metricsapi.utils
 
 val PORT = env("PORT")?.toIntOrNull() ?: 8080
+val MONGO_ADDRESS = env("MONGO_ADDRESS") ?: "mongodb://localhost"
