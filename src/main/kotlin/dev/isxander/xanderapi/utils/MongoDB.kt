80fa912c4424606c268c5083d48f626975468341
isXander <xandersmith2008@gmail.com>
2022-03-07 21:21:09 +0000

    fix mongo client not pointing to set address

--- src/main/kotlin/dev/isxander/xanderapi/utils/MongoDB.kt
+++ src/main/kotlin/dev/isxander/xanderapi/utils/MongoDB.kt
@@ -5,7 +5,7 @@ import com.mongodb.client.MongoDatabase
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
 
-val mongoClient = KMongo.createClient()
+val mongoClient = KMongo.createClient(MONGO_ADDRESS)
 
 inline fun <reified T : Any> MongoDatabase.getButDontCreateCollection(name: String): MongoCollection<T>? {
     if (this.listCollectionNames().any { it == name }) {
