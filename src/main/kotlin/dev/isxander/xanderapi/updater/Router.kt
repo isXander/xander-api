01a4f3aeb35b23b9f42291a219c1cef868d52a16
isXander <xandersmith2008@gmail.com>
2022-03-07 21:31:27 +0000

    authenticate /updater/new/{application} route

--- src/main/kotlin/dev/isxander/xanderapi/updater/Router.kt
+++ src/main/kotlin/dev/isxander/xanderapi/updater/Router.kt
@@ -4,6 +4,7 @@ import dev.isxander.xanderapi.response.GenericSuccessResponse
 import dev.isxander.xanderapi.response.VersionResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -18,13 +19,15 @@ fun Route.configureUpdaterRouting() {
         call.respond(HttpStatusCode.OK, VersionResponse(latest != null, latest))
     }
 
-    get("/updater/new/{application}") {
-        val application = call.parameters["application"] ?: throw BadRequestException("Application name is missing")
-        val loader = call.request.queryParameters["loader"] ?: throw BadRequestException("Loader is missing")
-        val minecraft = call.request.queryParameters["minecraft"] ?: throw BadRequestException("Minecraft version is missing")
-        val version = call.request.queryParameters["version"] ?: throw BadRequestException("Version is missing")
+    authenticate("auth-jwt") {
+        get("/updater/new/{application}") {
+            val application = call.parameters["application"] ?: throw BadRequestException("Application name is missing")
+            val loader = call.request.queryParameters["loader"] ?: throw BadRequestException("Loader is missing")
+            val minecraft = call.request.queryParameters["minecraft"] ?: throw BadRequestException("Minecraft version is missing")
+            val version = call.request.queryParameters["version"] ?: throw BadRequestException("Version is missing")
 
-        UpdaterManager.putVersion(application, UpdatableEntry(loader, minecraft, version))
-        call.respond(HttpStatusCode.OK, GenericSuccessResponse(true))
+            UpdaterManager.putVersion(application, UpdatableEntry(loader, minecraft, version))
+            call.respond(HttpStatusCode.OK, GenericSuccessResponse(true))
+        }
     }
 }
